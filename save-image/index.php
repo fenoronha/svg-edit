e8e5a377b045dd83af81faaf2a01923895c08dc9
Fernando Noronha <fenoronha@gmail.com>
2012-01-17 23:33:09 -0200

    nome do arquivo randomico

--- save-image/index.php
+++ save-image/index.php
@@ -5,5 +5,5 @@ $str = $_REQUEST['img'];
 $arr = explode(',', $str); 
 $data = $arr[1];
 header("Content-type: image/png");
-file_put_contents('imagem.png', base64_decode($data));
+file_put_contents('imagem-'.date(YmdHis).'.png', base64_decode($data));
 ?>
